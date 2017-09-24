e8f5edce2cbf29cccbdfbbb9f01b00dc2264e777
Bogdan Olteanu <olteanu.bogdan@gmail.com>
2017-09-24 17:13:43 +0300

    Added modified babelrc file, created scripts for npm and modified development-server file

--- build_scripts/development-server.js
+++ build_scripts/development-server.js
@@ -7,13 +7,13 @@ const port = 3000;
 const app = express();
 
 app.get('/', function (req, res) {
-  rs.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost', port);
+    open('http://localhost' + port);
   }
 });
