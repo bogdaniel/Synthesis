b3c55ca77f06d4c07df1b5e93f85d38b1e7b5000
Bogdan Olteanu <olteanu.bogdan@gmail.com>
2017-09-26 11:45:35 +0300

    Added a source map plugin for webpack and some test files

---
+++ webpack.config.dev.js
@@ -0,0 +1,66 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].map',
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              minimize: true,
+              sourceMap: true,
+            },
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
